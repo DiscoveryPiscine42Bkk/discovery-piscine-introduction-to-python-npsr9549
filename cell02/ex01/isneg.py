2a68707da19730e2430d34a3631c1be2399682b6
npsr9549 <napatsara.nchr@gmail.com>
2025-06-17 07:19:15 +0000

    cell02/ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = float(input("Please enter a number: "))
+if number >0:
+    print ("This number is nagative")
+if number <0:
+    print ("This number is positive")
+if number ==0:
+    print ("this number is both nagative and positive")
