01216cae7b473f5a4166fcd4a87e50ff66c5f9cc
npsr9549 <napatsara.nchr@gmail.com>
2025-06-18 10:32:49 +0000

    ghgh

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -2,7 +2,7 @@ import sys
 if len(sys.argv) != 2:
     print("none")
 else:
-    user_input = input("hello: ")
+    user_input = input("Enter a word: ")
     if user_input == sys.argv[1]:
         print("Good job!")
     else:
